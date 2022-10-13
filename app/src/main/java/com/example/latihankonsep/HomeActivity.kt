47114894efb41253bfbdd85011c3a84128c83774
achmad2002 <19kaizen@gmail.com>
2022-10-13 09:06:15 +0700

    intent with data

--- app/src/main/java/com/example/latihankonsep/HomeActivity.kt
+++ app/src/main/java/com/example/latihankonsep/HomeActivity.kt
@@ -1,5 +1,7 @@
 package com.example.latihankonsep
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.latihankonsep.databinding.ActivityHomeBinding
@@ -16,6 +18,11 @@ class HomeActivity : AppCompatActivity() {
         supportActionBar?.hide()
         homeActivityHomeBinding.edtStatus.text =
             "${intent.getStringExtra(NAME)}, apa yang anda pikirkan?"
+
+        homeActivityHomeBinding.call.setOnClickListener {
+            val i = Intent(Intent.ACTION_DIAL, Uri.parse("tel:0895384252730"))
+            startActivity(i)
+        }
 
     }
 
