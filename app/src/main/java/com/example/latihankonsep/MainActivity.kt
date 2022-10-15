9cc2d65db2a78be29be71d47f41aad1e86d6ee10
achmad2002 <19kaizen@gmail.com>
2022-10-15 19:30:08 +0700

    Add glide library

--- app/src/main/java/com/example/latihankonsep/MainActivity.kt
+++ app/src/main/java/com/example/latihankonsep/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.Navigation
 import com.example.latihankonsep.databinding.ActivityMainBinding
 
 const val NAME = "com.latihankonsep.app"
